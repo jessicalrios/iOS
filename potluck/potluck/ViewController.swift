8daaa4bd6f89206b2b3901c6a791976a0c34423b
Jessica Rios <jerios@csumb.edu>
2019-10-21 16:48:03 -0700

    added Firebasechat

---
+++ potluck/potluck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Potluck
+//
+//  Created by Jessica Rios on 10/18/19.
+//  Copyright © 2019 Jessica Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
